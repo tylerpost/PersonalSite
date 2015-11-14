e984bb7fa2d455b671b4da678b4020cbc5eea412
Tyler Post <tyler.post200@gmail.com>
2015-11-13 22:55:58 -0500

    hosting configured

--- Projects/projects.js
+++ Projects/projects.js
@@ -1,1 +1,15 @@
 //Controller for projects page
+
+
+angular.module('mySite.projects', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/projects', {
+    templateUrl: 'Projects/projects.html',
+    controller: 'projectsCtrl'
+  });
+}])
+
+.controller('projectsCtrl', [function() {
+	
+}]);
--- home.js
+++ home.js
@@ -1,1 +1,13 @@
 //Main controller including page routing
+
+
+angular.module('mySite', [
+  'ngRoute',
+  'mySite.projects',
+  'mySite.resume'
+]).
+
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.otherwise({redirectTo: '/'});
+  console.log("PLS");
+}]);
